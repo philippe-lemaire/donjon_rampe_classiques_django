8c376bc26b01400a3a206dd57f0584e481560cb4
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-11-14 10:57:37 +0100

    cleric spells ok

--- game_data/urls.py
+++ game_data/urls.py
@@ -6,4 +6,5 @@ app_name = "game_data"
 
 urlpatterns = [
     path("/sorts_de_mage", views.view_wizard_spells, name="view_wizard_spells"),
+    path("/sorts_de_clerc", views.view_cleric_spells, name="view_cleric_spells"),
 ]
--- game_data/views.py
+++ game_data/views.py
@@ -11,3 +11,11 @@ def view_wizard_spells(request):
     return render(
         request, template_name="game_data/view_wizard_spells.html", context=context
     )
+
+
+def view_cleric_spells(request):
+    context = {"spells": cleric_spells}
+
+    return render(
+        request, template_name="game_data/view_cleric_spells.html", context=context
+    )
